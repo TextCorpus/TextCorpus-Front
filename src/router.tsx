b9aebd7d31fcfd5ec64a6f79b6e5c759416111bc
Ryan Pizani <rypizani@gmail.com>
2024-03-13 21:27:00 -0300

    teste

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import PasswordRecovery from './pages/PasswordRecovery/PasswordRecovery';
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Adicione esta linha e ajuste conforme necessário
   plugins: [react()],
 })
