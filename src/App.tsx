e76855bca9491adfecacbffce5e0938c4c2e0988
Ryan Pizani <rypizani@gmail.com>
2024-03-13 23:08:22 -0300

    sem vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import AppRouter from './router';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./pages/router";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <AppRouter />
+      </div>
+    </BrowserRouter>
+  );
 }
+
 export default App;
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/', // Adicione esta linha e ajuste conforme necessário
-  plugins: [react()],
-})
