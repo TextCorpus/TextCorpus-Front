896184d0e12f237fa301abce68f911955f6a63bc
Ryan Pizani <rypizani@gmail.com>
2024-03-28 14:47:07 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./pages/router";
+import AppRouter from './router';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <AppRouter />
-      </div>
-    </BrowserRouter>
-  );
+  return <AppRouter />;
 }
-
 export default App;
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Login from './Login/Login';
-import PasswordRecovery from './PasswordRecovery/PasswordRecovery';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import PasswordRecovery from './pages/PasswordRecovery/PasswordRecovery';
 
 const AppRouter: React.FC = () => {
   return (
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/esqueci-minha-senha" element={<PasswordRecovery />} />
       </Routes>
+    </Router>
   );
 }
 
